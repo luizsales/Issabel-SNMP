349e6d1322f159226e5b4ff2368489b4869e9653
Luiz Sales <quick.br@gmail.com>
2016-06-20 17:07:39 -0300

    Update move file

--- digivoice.sh
+++ digivoice.sh
@@ -27,3 +27,23 @@ tar zxvf voicerlib-4.2.5.6.tar.gz
 
 tar zxvf dgvchannel-1.1.8.tar.gz
 
+mv asterisk-1.8.32.3 asterisk
+mv dahdi-linux-complete-2.11.1+2.11.1 dahdi-linux-complete
+mv dgvchannel-1.1.8 dgvchannel
+mv libpri-1.5.0 libpri
+mv voicerlib-4.2.5.6 voicerlib
+
+mkdir asterisk_package
+
+mv libpri-current.tar.gz asterisk_package
+
+mv dahdi-linux-complete-current.tar.gz asterisk_package
+
+mv asterisk-1.8-current.tar.gz asterisk_package
+
+mv voicerlib-4.2.5.6.tar.gz asterisk_package
+
+mv dgvchannel-1.1.8.tar.gz asterisk_package
+
+
+
